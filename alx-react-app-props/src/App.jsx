c758de68263b93b98e6b5c68cf03dc53d5d83bbf
mstf-ai <albrnsmstfy252@gmail.com>
2025-12-26 04:32:29 +0200

    new commit

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,15 +1,15 @@
-import ProfilePage from "./ProfilePage";
 import UserContext from "./context/UserContext";
+import UserProfile from "./UserProfile";
 
 function App() {
-  const userData = {
-    name: "Jane Doe",
-    email: "jane.doe@example.com",
+  const user = {
+    name: "John Doe",
+    email: "john.doe@example.com",
   };
 
   return (
-    <UserContext.Provider value={userData}>
-      <ProfilePage />
+    <UserContext.Provider value={user}>
+      <UserProfile />
     </UserContext.Provider>
   );
 }
