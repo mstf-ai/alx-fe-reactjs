9409e481c2ae4c12cd2187a43851954c19543adf
mstf-ai <albrnsmstfy252@gmail.com>
2025-12-26 21:58:34 +0200

    new commit

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -6,7 +6,12 @@ function UserProfile() {
         sm:p-4 md:p-8
         max-w-xs md:max-w-sm
         mx-auto my-20
-        rounded-lg shadow-lg
+        rounded-lg
+        shadow-lg
+        hover:shadow-xl
+        transition-shadow
+        duration-300
+        ease-in-out
         text-center
       "
     >
@@ -18,6 +23,10 @@ function UserProfile() {
           sm:w-24 sm:h-24
           md:w-36 md:h-36
           mx-auto
+          hover:scale-110
+          transition-transform
+          duration-300
+          ease-in-out
         "
       />
 
@@ -26,6 +35,9 @@ function UserProfile() {
           sm:text-lg md:text-xl
           text-blue-800
           my-4
+          hover:text-blue-500
+          transition-colors
+          duration-300
         "
       >
         John Doe
