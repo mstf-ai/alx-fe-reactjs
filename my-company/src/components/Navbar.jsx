5d8d5d98d54dd26df7c7a2c9ecbd94978c36fcb7
mstf-ai <albrnsmstfy252@gmail.com>
2025-12-26 04:54:20 +0200

    new commit

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,14 +2,30 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{
-      padding: '10px',
-      backgroundColor: '#333'
-    }}>
-      <Link to="/" style={{ color: '#fff', marginRight: '10px' }}>Home</Link>
-      <Link to="/about" style={{ color: '#fff', marginRight: '10px' }}>About</Link>
-      <Link to="/services" style={{ color: '#fff', marginRight: '10px' }}>Services</Link>
-      <Link to="/contact" style={{ color: '#fff' }}>Contact</Link>
+    <nav
+      style={{
+        display: 'flex',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        padding: '15px',
+        backgroundColor: '#222',
+      }}
+    >
+      <Link style={{ color: 'white', textDecoration: 'none' }} to="/">
+        Home
+      </Link>
+
+      <Link style={{ color: 'white', textDecoration: 'none' }} to="/about">
+        About
+      </Link>
+
+      <Link style={{ color: 'white', textDecoration: 'none' }} to="/services">
+        Services
+      </Link>
+
+      <Link style={{ color: 'white', textDecoration: 'none' }} to="/contact">
+        Contact
+      </Link>
     </nav>
   );
 }
