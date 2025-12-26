f2924b9cc3b2ddc7da687cceb8f435fb8bd7f1fc
mstf-ai <albrnsmstfy252@gmail.com>
2025-12-26 04:48:13 +0200

    new commit

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,21 +1,23 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Home from './Home';
 import About from './About';
 import Services from './Services';
 import Contact from './Contact';
-import Navbar from './Navbar';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
